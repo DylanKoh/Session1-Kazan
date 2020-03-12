7abd694423b4c518d1c527cf05984ff70c5637a0
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-03-12 14:40:25 +0800

    Fixed UI

--- Session1-Kazan/EditPage.xaml.cs
+++ Session1-Kazan/EditPage.xaml.cs
@@ -270,12 +270,10 @@ namespace Session1_Kazan
             }
         }
 
-        private async Task btnCapture_ClickedAsync(object sender, EventArgs e)
+        private async void btnCapture_Clicked(object sender, EventArgs e)
         {
             var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
-            
-            if (photo != null)
-                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+
         }
 
         private void btnBrowse_Clicked(object sender, EventArgs e)
